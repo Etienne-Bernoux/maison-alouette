1b3bcb37f947e9896e46ff8f29002428bdf27867
etiennebernoux <ebernoux@invivo-group.com>
2021-04-09 10:41:04 +0200

    conformité gdpr

---
+++ assets/js/gdpr.js
@@ -0,0 +1,11 @@
+window.axeptioSettings = {
+  clientId: '607011683472a46f897a60c4',
+  cookiesVersion: 'maison-alouette-base',
+};
+
+(function (d, s) {
+  const t = d.getElementsByTagName(s)[0], e = d.createElement(s)
+  e.async = true
+  e.src = '//static.axept.io/sdk.js'
+  t.parentNode.insertBefore(e, t)
+})(document, 'script')
